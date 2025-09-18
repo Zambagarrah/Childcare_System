7e59d92c4af4d598786c37fe29eaabf65e2cf23e
Zambagarrah <zambagarrah@users.noreply.github.com>
2025-09-19 00:53:27 +0300

    feat: Add route for CaregiverDashboard component

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Home from '../pages/Home';
 import Login from '../components/Auth/Login';
 import Register from '../components/Auth/Register';
 import ParentDashboard from '../components/Dashboard/ParentDashboard';
+import CaregiverDashboard from '../components/Dashboard/CaregiverDashboard';
 
 export default function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ export default function AppRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/parent-dashboard" element={<ParentDashboard />} />
+        <Route path="/caregiver-dashboard" element={<CaregiverDashboard />} />
       </Routes>
     </Router>
   );
